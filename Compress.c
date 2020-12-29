0cc2013695210082a080aadf3c5f91d4bbe1a91b
chen-xuyuan <chenxuyuan.cxy@gmail.com>
2020-12-29 05:41:33 -0800

    framework

--- Compress.c
+++ Compress.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+int tar()
+{
+    return 0;
+}
+
+int untar()
+{
+    return 0;
+}
+
+int huffman()
+{
+    return 0;
+}
+
+int compress()
+{
+    return 0;
+}
+
+int uncompress()
+{
+    return 0;
+}
 
 int main()
 {
