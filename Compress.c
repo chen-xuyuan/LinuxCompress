05ad4c6fbbbc7e387dce6d12a896b1bace77cf7b
chen-xuyuan <chenxuyuan.cxy@gmail.com>
2020-12-30 18:45:56 +0800

    add some functions

--- Compress.c
+++ Compress.c
@@ -4,6 +4,53 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define REGULAR    0
+#define NORMAL    '0'
+#define HARDLINK  '1'
+#define SYMLINK   '2'
+#define CHAR      '3'
+#define BLOCK     '4'
+#define DIRECTORY '5'
+#define FIFO      '6'
+
+typedef union Record
+{
+	union
+	{
+		// Pre-POSIX.1-1988 format
+		struct
+		{
+			char name[100];      // file name
+			char mode[8];        // permissions
+			char uid[8];         // user id (octal)
+			char gid[8];         // group id (octal)
+			char size[12];       // size (octal)
+			char mtime[12];      // modefication time (octal)
+			char check[8];       // sum of unsigned characters in block (octal)
+			char link;           // link indicator
+			char link_name[100]; // name of linked file
+		};
+
+		// UStar format (POSIX IEEE P1003.1)
+		struct
+		{
+			char old[156];            // first 156 octets of Pre-POSIX.1-1998 format
+			char type;                // file type
+			char also_link_name[100]; // name of linked file
+			char ustar[8];            // ustar\000
+			char owner[32];           // user name (string)
+			char group[32];           // group name (string)
+			char major[8];            // device major number
+			char minor[8];            // device minor number
+			char prefix[155];
+		};
+	};
+
+	char block[512]; // raw memory (padded to 1 block)
+}Record;
+
+int Frequency[256] = {0};
+
 char *mallocAndReset(size_t length)
 {
     char *p = (char*)malloc(length);
@@ -16,7 +63,24 @@ char *mallocAndReset(size_t length)
     return p;
 }
 
-int tar(char *path)
+void countFrequency(Record *block)
+{
+    for (int i = 0;i<512;i++) Frequency[((char *)block)[i]]++;
+}
+
+void copyName(char *path,Record *block)
+{
+    char *content = (char *)block;
+    int copyLength = strlen(path) < 100 ? strlen(path) : 100;
+    for(int i=0;i<copyLength;i++) content[i] = path[i];
+}
+
+int tarLongName(char *path,FILE *fout)
+{
+    return 0;
+}
+
+int tar(char *path,FILE *fout)
 {
     struct stat statBuf;
     if (stat(path,&statBuf))
@@ -26,6 +90,13 @@ int tar(char *path)
     }
     if (S_ISDIR(statBuf.st_mode))
     {
+        if (strlen(path) + 1 > 100)
+        {
+            char *dirPath = (char *)malloc(strlen(path)+2);
+            strcat(dirPath,path);
+            strcat(dirPath,"/");
+            tarLongName(dirPath,fout);
+        }
         DIR * dirPoint = opendir(path);
         if (!dirPoint)
         {
@@ -40,7 +111,7 @@ int tar(char *path)
             strcat(nextPath,path);
             if (strcmp("/",path)) strcat(nextPath,"/"); // if dir is "/" don't add /
             strcat(nextPath,dirSata->d_name);
-            tar(nextPath);
+            tar(nextPath,fout);
             free(nextPath);
         }
         printf("%s/\n",path);
@@ -48,6 +119,7 @@ int tar(char *path)
     }
     else
     {
+        if (strlen(path) > 100) tarLongName(path,fout);
         printf("%s\n",path);
     }
     
@@ -77,7 +149,19 @@ int uncompress()
 int main()
 {
     char path[] = "/home/ricksanchez/test";
+    char tarPath[] = "/home/ricksanchez/test.tar";
+
     if (path[strlen(path)-1] == '/' && strlen(path) > 1) path[strlen(path)-1] = '\0'; // if path end of '/' and path is not "/" or "."
-    tar(path);
+
+    FILE *fout = fopen(tarPath,"wb");
+    if (!fout)
+    {
+        perror("fopen");
+        return 1;
+    }
+
+    tar(path,fout);
+
+    fclose(fout);
     return 0;
 }
