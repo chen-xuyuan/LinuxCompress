11ef3280d1c6b94ced6beb577da5ac8b0d4d4605
chen-xuyuan <chenxuyuan.cxy@gmail.com>
2020-12-29 23:02:18 -0800

    get dir

--- Compress.c
+++ Compress.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include <string.h>
 
-int tar()
+char *mallocAndReset(size_t length)
 {
+    char *p = (char*)malloc(length);
+    if (!p)
+    {
+        perror("no enough space to malloc");
+        exit(1);
+    }
+    memset(p,0,length);
+    return p;
+}
+
+int tar(char *dirPath)
+{
+    printf("%s\n",dirPath);
+    DIR *dirPoint = opendir(dirPath);
+    if (!dirPoint)
+    {
+        perror("open dir error");
+        exit(1);
+    }
+    struct dirent *fileStat;
+    while(fileStat = readdir(dirPoint))
+    {
+        char *nextPath = mallocAndReset(strlen(dirPath) + 2 + strlen(fileStat->d_name));
+        strcat(nextPath,dirPath);
+        strcat(nextPath,fileStat->d_name);
+        if (fileStat->d_type == DT_DIR && strcmp(".",fileStat->d_name) && strcmp("..",fileStat->d_name))
+        {
+            strcat(nextPath,"/");
+            tar(nextPath);
+        }
+        else
+        {
+            printf("%s\n",nextPath);
+        }
+        free(nextPath);
+    }
     return 0;
 }
 
@@ -28,6 +66,7 @@ int uncompress()
 
 int main()
 {
-    printf("Hello,World!\n");
+    char path[] = "/home/ricksanchez/test/";
+    tar(path);
     return 0;
 }
